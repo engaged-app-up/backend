0529ee15e57529ab43075fbba25674cdcf32dc08
itrytocodethings <wbaileydev@gmail.com>
2022-08-19 14:24:33 -0400

    added sentry

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const Sentry = require('@sentry/node');
 
-app.use(cors())
+Sentry.init({dsn: "https://3609947d29744121845b0f85c5a23100@o1368148.ingest.sentry.io/6670539"});
+app.use(Sentry.Handlers.requestHandler());
 
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.status(200).json({msg: 'Hello World'});
+    // throw new Error('testing sentry');
+    res.status(200).json({msg: 'Hello World'});
 })
+
+app.use(Sentry.Handlers.errorHandler());
 
 const port = 3001
 app.listen(port, () => {
