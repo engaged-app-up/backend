754c72b50bf70cefa9d931b7afa09b5ea82ea2d8
itrytocodethings <wbaileydev@gmail.com>
2022-09-22 09:23:42 -0400

    WIP: room creation/joining backend

--- controllers/room-controller.js
+++ controllers/room-controller.js
@@ -2,8 +2,10 @@ const prisma = require('../db/prisma');
 const HttpError = require('../util/http-error');
 const {getAuth} = require('firebase-admin/auth');
 const { user } = require('../db/prisma');
+const { v4: uuidv4 } = require('uuid');
 
 const createRoom = async (req, res, next) => {
+    console.log(req.headers.uid);
     const {name, description} = req.body;
 
     //get user trying to create room.
@@ -12,32 +14,28 @@ const createRoom = async (req, res, next) => {
          user = await prisma.user.findUnique({
             where: {
                 uid: req.headers.uid
-            },
-            include: {
-                ownedRooms: true,
             }
         })
-        console.log(user);
     } catch (error) {
         return next(new HttpError('Cannot create room. User not found.', 500))
     }
 
+    let room;
     try {
-        const room = await prisma.room.create({
+        room = await prisma.room.create({
             data: {
+                uuid: uuidv4(),
                 name,
                 description,
                 creatorId: user.id
             }
         })
-        console.log(room);
-        console.log(user.ownedRooms);
     } catch (error) {
         return next(new HttpError('Failed to create room.\n' + error, 500));
     }
 
     //create room - TODO
-    res.json({'msg': 'create room!'});
+    res.json(room);
 };
 
 const deleteRoom = async (req, res, next) => {
@@ -45,7 +43,49 @@ const deleteRoom = async (req, res, next) => {
 };
 
 const joinRoom = async (req, res, next) => {
-    //todo
+    //get user requesting to join
+    let user;
+    try {
+        user = await prisma.user.findUnique({
+            where: {
+                uid: req.headers.uid
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return next(new HttpError('Failed to find user', 500));
+    }
+
+    let room;
+    try {
+        room = await prisma.room.findUnique({
+            where: {
+                id: req.body.roomId
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return next(new HttpError('Failed to find room', 500));
+    }
+
+    try {
+        await prisma.room.update({
+            where: {
+                id: room.id
+            },
+            data: {
+                members: {
+                    connect: {
+                        id: user.id
+                    }
+                }
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return next(new HttpError('failed', 500));
+    }
+    res.json({msg: 'join successful'});
 };
 
 const leaveRoom = async (req, res, next) => {
--- routes/room-routes.js
+++ routes/room-routes.js
@@ -3,7 +3,28 @@ const router = express.Router();
 const fbAuth = require('../middleware/fbAuth');
 // controller
 const roomController = require('../controllers/room-controller');
+const prisma = require('../db/prisma');
+const HttpError = require('../util/http-error');
 
 router.post('/create', fbAuth, roomController.createRoom);
+router.post('/join', fbAuth, roomController.joinRoom);
+router.get('/testing', fbAuth, async (req, res, next) => {
+    let user;
+    try {
+        user = await prisma.user.findUnique({
+            where: {
+                uid: req.headers.uid
+            },
+            include: {
+                rooms: true
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        return next(new HttpError('Error', 500));
+    }
+
+    res.json(user);
+})
 
 module.exports = router;
